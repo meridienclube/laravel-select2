8eb45c47ce4be27007cc4306efa5182e560312f5
rafazingano <rafazingano@gmail.com>
2020-02-13 18:01:46 -0300

    Update all

--- src/Views/components/form/select2.blade.php
+++ src/Views/components/form/select2.blade.php
@@ -1,40 +1,46 @@
+@php
+    $attributes['id'] = $attributes['id']?? mt_rand();
+@endphp
+
 {{ Form::select($name, $values, $selected, $attributes) }}
 
 @push('scripts')
 
         <script>
-            @isset($select2Attr['server_side'])
-            $('#{{ $attributes['id'] }}').select2({
-                dropdownAutoWidth : true,
-                width: 'resolve',
-                allowClear: false,
-                placeholder: "{{ $attributes['placeholder'] ?? 'Selecione' }}",
-                required: "{{ $attributes['required'] ?? false }}",
-                minimumInputLength: 3,
-                ajax: {
-                    url: "{{ route($select2Attr['server_side']['route'], ['api_token' => auth()->user()->api_token]) }}",
-                    dataType: "json",
-                    delay: "{{ $attributes['delay'] ?? 400 }}",
-                    data: function (params) {
-                        return {
-                            @isset($select2Attr['getValues'])
-                                @foreach($select2Attr['getValues'] as $k => $v)
-                                    {!! $k . ': $("' . $v . ' option:selected").val(),' !!}
-                                @endforeach
-                            @endisset
-                            term: params.term
+            $(document).ready(function() {
+                @isset($select2Attr['server_side'])
+                $('#{{ $attributes['id']  }}').select2({
+                    dropdownAutoWidth: true,
+                    width: 'resolve',
+                    allowClear: false,
+                    placeholder: "{{ $attributes['placeholder'] ?? 'Selecione' }}",
+                    required: "{{ $attributes['required'] ?? false }}",
+                    minimumInputLength: 3,
+                    ajax: {
+                        url: "{{ route($select2Attr['server_side']['route'], ['api_token' => auth()->user()->api_token]) }}",
+                        dataType: "json",
+                        delay: "{{ $attributes['delay'] ?? 400 }}",
+                        data: function (params) {
+                            return {
+                                @isset($select2Attr['getValues'])
+                                        @foreach($select2Attr['getValues'] as $k => $v)
+                                        {!! $k . ': $("' . $v . ' option:selected").val(),' !!}
+                                        @endforeach
+                                        @endisset
+                                term: params.term
+                            }
+                        },
+                        processResults: function (data) {
+                            return {
+                                results: data.data
+                            };
                         }
-                    },
-                    processResults: function (data) {
-                        return {
-                            results: data.data
-                        };
                     }
-                }
+                });
+                @else
+                $('#{{ $attributes['id'] }}').select2();
+                @endisset
             });
-            @else
-            $('#{{ $attributes['id'] }}').select2();
-            @endisset
         </script>
 
 @endpush
